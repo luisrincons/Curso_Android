4cddbfd4899ccb7496d95c2f0d784634bfbddba2
luisrincons <luisrincons@gmail.com>
2014-10-04 14:16:14 -0500

    ver 1.3

--- js/api/capture.js
+++ js/api/capture.js
@@ -3,13 +3,11 @@ var myCapture = {
     tomarFoto: function(){
         navigator.device.capture.captureImage(myCapture.fotoTomada,myCapture.fotoError);
     },
-    
     fotoTomada: function(fotos){
-        var path = foto[0].fullPath;
-        $('#regTake').attr('rel',path); //Jquery
+        var path = fotos[0].fullPath;
+        $('#regTake').attr('rel',path);
         $('#foto').html('<img src="'+path+'" style="width:100%;">');
     },
-    
     fotoError: function(err){
         navigator.notification.alert('Error: '+err.code,null,'Error','Aceptar');
     }
