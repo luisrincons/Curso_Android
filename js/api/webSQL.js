7a7d3c2bca9cd98cfc5d6ed5393223805b29bec9
luisrincons <luisrincons@gmail.com>
2014-10-18 13:41:57 -0500

    v1.8.8

--- js/api/webSQL.js
+++ js/api/webSQL.js
@@ -10,26 +10,12 @@ var db = {
         db.di = di;
         db.crearDB().transaction(db.tablaPendientes,db.error,db.exitoPendientes);
     },
-    agregarHistorial: function(th,ha,pr,di){
-        db.th = th;
-        db.ha = ha;
-        db.pr = pr;
-        db.di = di;
-        db.crearDB().transaction(db.tablaHistorial,db.error,db.exitoHistorial);
-    },
     tablaPendientes: function(tx){
         tx.executeSql("CREATE TABLE IF NOT EXISTS pendientes (id unique, th, ha, pr, di)");
         tx.executeSql("INSERT INTO pendientes (th, ha, pr, di) VALUES ('" + db.th + "', '" + db.ha + "', '" + db.pr + "', '" + db.di + "')");
     },
     exitoPendientes: function(){
         navigator.notification.alert('Reserva en espera de conexión',null,'Guardado','Aceptar');
-    },
-    tablaHistorial: function(tx){
-        tx.executeSql("CREATE TABLE IF NOT EXISTS historial (id unique, th, ha, pr, di)");
-        tx.executeSql("INSERT INTO historial (th, ha, pr, di) VALUES ('" + db.th + "', '" + db.ha + "', '" + db.pr + "', '" + db.di + "')");
-    },
-    exitoHistorial: function(){
-        navigator.notification.alert('Proceso concluido.',null,'Guardado','Aceptar');
     },
     error: function(err){
         alert('Error: '+err.code);
