9ba2b7d818bef97aa4bc67d2e70c7dd2d0b5ef3c
luisrincons <luisrincons@gmail.com>
2014-10-18 13:01:32 -0500

    v1.8.3

--- js/api/webSQL.js
+++ js/api/webSQL.js
@@ -3,21 +3,21 @@ var db = {
         var obj = window.openDatabase("hotel", "1.0", "Hotel Demo", 200000);
         return obj;
     },
-    agregarPendientes: function (th, ha, pr, di){
+    agregarPendientes: function(th,ha,pr,di){
         db.th = th;
         db.ha = ha;
         db.pr = pr;
         db.di = di;
-        db.crearDB().transaction(db.tablaPendientes, db.error, db.exitoPendientes);
+        db.crearDB().transaction(db.tablaPendientes,db.error,db.exitoPendientes);
     },
     tablaPendientes: function(tx){
-        tx.executeSql("CREATE TABLE IF NOT EXISTS pendientes (id unique,  th, ha, pr, di)");
-        tx.executeSql("INSERT INTO pendientes (th, ha, pr, di) VALUES ('" + db.th + "', '" + db.ha + "', '" + db.pr + "', '" + db.di + "')');
+        tx.executeSql("CREATE TABLE IF NOT EXISTS pendientes (id unique, th, ha, pr, di)");
+        tx.executeSql("INSERT INTO pendientes (th, ha, pr, di) VALUES ('" + db.th + "', '" + db.ha + "', '" + db.pr + "', '" + db.di + "')");
     },
     exitoPendientes: function(){
         navigator.notification.alert('Reserva en espera de conexión',null,'Guardado','Aceptar');
     },
     error: function(err){
-        alert('Error' + err.code);
+        alert('Error: '+err.code);
     }
 }
